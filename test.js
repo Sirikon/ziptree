f62beb75268456dd7226b8310e390e4911b80d1c
Carlos Fernández Llamas <sirikon2@gmail.com>
2014-03-23 14:24:42 +0100

    Fix #2, Fix #3

--- test.js
+++ test.js
@@ -5,9 +5,16 @@ var ziptree = require('./lib/ziptree.js');
 var foto = fs.readFileSync(process.cwd() + '/foto.jpg');
 
 var file = new ziptree({
-	'prueba1.txt': 'Hello Moto',
-	'prueba2.txt': 23303030,
-	'fotoguay.jpg': foto
+	'testfile1.txt': 'This is a String',
+	'testfile2.txt': 123456,
+	'photo.jpg': foto,
+	'folder': {
+		'samephoto.jpg': foto,
+		'im_gonna_get_you_too.txt': 'Another one bites the dust...',
+		'folder-ception': {
+			'photoagain.jpg': foto
+		}
+	}
 });
 
 var data = file.toBuffer();
